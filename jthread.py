21a76706b6a1a923c0de71ce3e3f5dd2d0635480
Jhe <ucg.jhe@gmail.com>
2015-03-28 15:34:39 +0800

    adjust pushback function

--- jthread.py
+++ jthread.py
@@ -8,40 +8,51 @@ threadPool = Queue.Queue(0)
 
 condition = Condition()
 
+found = []
 
+PUSHBACK = []
 
 class JThread(Thread):
-    def __init__(self, condition, target, port):
+    def __init__(self, condition, target, port, verbose=False):
         Thread.__init__(self)
         self.cond = condition
         self.target = target
         self.port = port
+        self.counter = 3
+        self.verbose = verbose
     def run(self):
         while True:
-            global target
-
-            each = threadPool.get()
+            global found, PUSHBACK
             
-            if each.startswith('.'):
+            each = threadPool.get()
+
+            if each.endswith('/'):
+                each = each.strip('/') + '/'
+            elif each.startswith('.'):
                 each = each + '/'
             elif '.' not in each:
                 each = each + '/'
                 
             url = '%s:%s/%s' % (self.target, self.port, each)
+
             
             try:
-                res = urllib2.urlopen(url, timeout=3)
+                res = urllib2.urlopen(url, timeout=10)
                 print '[+][%s] %s\n' % (res.code, url),
+                found.append(url)
             except urllib2.HTTPError as err:
-                if err.code != 404:
+                if self.verbose is True:
                     print '[-][%s] %s\n' % (err.code, url),
+                elif err.code != 404:
+                    print '[-][%s] %s\n' % (err.code, url),
+                    found.append(url)
             except (socket.timeout, urllib2.URLError) as err:
-                print '[Timeout][%s]' % (url)
-            finally:
-                self.cond.acquire()
-                self.cond.release()
-                threadPool.task_done()
-
+                print 'PUSHBACK %s\n' % (each),
+                PUSHBACK.append(each)
+            self.cond.acquire()
+            self.cond.release()
+            threadPool.task_done()
+            
 def main():
     pass
 
