e4760ea4c802802a041a4e051ced690ee628f60b
Jhe <ucg.jhe@gmail.com>
2015-06-28 08:35:00 +0800

    change get method to head for speed

--- jthread.py
+++ jthread.py
@@ -22,11 +22,11 @@ class JThread(threading.Thread):
         self.target = target
         self.port = port
         self.verbose = verbose
-        
+
     def run(self):
         while True:
             global found, errlist
-            
+
             each = jobq.get()
 
             if each.endswith('/'):
@@ -35,40 +35,40 @@ class JThread(threading.Thread):
                 each = each + '/'
             elif '.' not in each:
                 each = each + '/'
-                
+
             url = '%s:%s/%s' % (self.target, self.port, each)
 
             self.cond.acquire()
-            
+
             try:
-                res = requests.get(url, timeout=5)
+                res = requests.head(url, timeout=5)
 
                 if res.ok:
                     msg = '[+][%s] %s\n' % (res.status_code, url)
                     if res.history:
                         msg = '[-][%s] %s\n' % (res.history[0].status_code, url)
-                    
+
                 elif not res.ok:
                     msg = '[-][%s] %s\n' % (res.status_code, url)
-                    
+
                 if res.status_code != 404:
                     found.append(each)
                     print msg
-                    
+
             except requests.exceptions.Timeout as err:
                 print 'Timeout'
                 errlist.append('Timeout ' + each)
-                
+
             except socket.timeout as err:
                 errlist.append('Socket Timeout ' + each)
 
             except requests.exceptions.RequestException as err:
                 errlist.append('Unexpect Exception ' + str(err))
 
-            
+
             self.cond.release()
             jobq.task_done()
-            
+
 def main():
     pass
 
